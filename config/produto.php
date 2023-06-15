0329b6a5ea11adffca188591e130e051a62c1380
Kaue Felipe Sodate <99609927+KaueFSodate@users.noreply.github.com>
2023-06-14 23:06:25 -0300

    Update produto.php

--- config/produto.php
+++ config/produto.php
@@ -15,7 +15,7 @@ $products = array(
         'Nome' => 'Calça Jeans',
         'Valor' => 79.99,
         'quantidade' => 1,
-        'imagem' => 'https://i5.walmartimages.com/asr/07c38307-9678-4450-b190-c0b384f4ed6c_1.fe881ef2cc4bb838e516fff313c4b559.jpeg'
+        'imagem' => 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTju8r4USTTUX3nSObzI3qkcGrOZtqVvRkRzk8x_eq28BUbcJy5PhUn6Hz8hNGh87PiKig&usqp=CAU'
     ),
     array(
         'id' => $indice,
@@ -23,7 +23,7 @@ $products = array(
         'Nome' => 'Tênis',
         'Valor' => 99.99,
         'quantidade' => 1,
-        'imagem' => 'https://i5.walmartimages.com/asr/07c38307-9678-4450-b190-c0b384f4ed6c_1.fe881ef2cc4bb838e516fff313c4b559.jpeg'
+        'imagem' => 'https://imgcentauro-a.akamaihd.net/900x900/97360651/tenis-adidas-grand-court-2-0-feminino-img.jpg'
     ),
     array(
         'id' => $indice,
@@ -31,7 +31,7 @@ $products = array(
         'Nome' => 'Óculos de Sol',
         'Valor' => 59.99,
         'quantidade' => 1,
-        'imagem' => 'https://i5.walmartimages.com/asr/07c38307-9678-4450-b190-c0b384f4ed6c_1.fe881ef2cc4bb838e516fff313c4b559.jpeg'
+        'imagem' => 'https://images.tcdn.com.br/img/img_prod/810719/oculos_de_sol_oakley_quadrado_oo9018_ojector_3191_1_8d7b4a0542f8b5ebf1be0315fd8b4edb.jpg'
     ),
     array(
         'id' => $indice,
@@ -39,7 +39,7 @@ $products = array(
         'Nome' => 'Bolsa',
         'Valor' => 39.99,
         'quantidade' => 1,
-        'imagem' => 'https://i5.walmartimages.com/asr/07c38307-9678-4450-b190-c0b384f4ed6c_1.fe881ef2cc4bb838e516fff313c4b559.jpeg'
+        'imagem' => 'https://img.irroba.com.br/fit-in/600x600/filters:fill(fff):quality(80)/seltenbr/catalog/472-atlant-cart-rosa-ml-20.JPG'
     ),
     array(
         'id' => $indice,
@@ -47,7 +47,7 @@ $products = array(
         'Nome' => 'Relógio',
         'Valor' => 129.99,
         'quantidade' => 1,
-        'imagem' => 'https://i5.walmartimages.com/asr/07c38307-9678-4450-b190-c0b384f4ed6c_1.fe881ef2cc4bb838e516fff313c4b559.jpeg'
+        'imagem' => 'https://cea.vtexassets.com/arquivos/ids/55910600/RELOGIO-LED-QUADRADO-IMPORTADO-A-2-PRETO-1033155-Preto_1.jpg?v=638018710019500000'
     ),
     array(
         'id' => $indice,
@@ -55,7 +55,7 @@ $products = array(
         'Nome' => 'Chapéu',
         'Valor' => 19.99,
         'quantidade' => 1,
-        'imagem' => 'https://i5.walmartimages.com/asr/07c38307-9678-4450-b190-c0b384f4ed6c_1.fe881ef2cc4bb838e516fff313c4b559.jpeg'
+        'imagem' => 'https://images.tcdn.com.br/img/img_prod/646215/chapeu_ariat_20x_de_palha_2019_1_20201214093609.jpg'
     ),
     array(
         'id' => $indice,
@@ -63,7 +63,7 @@ $products = array(
         'Nome' => 'Saia',
         'Valor' => 49.99,
         'quantidade' => 1,
-        'imagem' => 'https://i5.walmartimages.com/asr/07c38307-9678-4450-b190-c0b384f4ed6c_1.fe881ef2cc4bb838e516fff313c4b559.jpeg'
+        'imagem' => 'https://dondoca.com.br/wp-content/uploads/2021/12/dondoca_com_br-img-3842.jpg'
     ),
     array(
         'id' => $indice,
@@ -71,7 +71,7 @@ $products = array(
         'Nome' => 'Jaqueta',
         'Valor' => 89.99,
         'quantidade' => 1,
-        'imagem' => 'https://i5.walmartimages.com/asr/07c38307-9678-4450-b190-c0b384f4ed6c_1.fe881ef2cc4bb838e516fff313c4b559.jpeg'
+        'imagem' => 'https://cdn.shopify.com/s/files/1/0393/1016/4013/products/jaqueta-oslo-preto-constantinni-p-m-g-gg_800x.png?v=1653394220'
     ),
     array(
         'id' => $indice,
@@ -79,7 +79,7 @@ $products = array(
         'Nome' => 'Sapato Social',
         'Valor' => 109.99,
         'quantidade' => 1,
-        'imagem' => 'https://i5.walmartimages.com/asr/07c38307-9678-4450-b190-c0b384f4ed6c_1.fe881ef2cc4bb838e516fff313c4b559.jpeg'
+        'imagem' => 'https://img.irroba.com.br/fit-in/600x600/filters:fill(fff):quality(80)/hypestor/catalog/sapato-social-masculino-em-couro-legitimo-ortopedico-cor-preto-hype840-2.jpg'
     ),
     array(
         'id' => $indice,
@@ -87,7 +87,7 @@ $products = array(
         'Nome' => 'Brincos',
         'Valor' => 14.99,
         'quantidade' => 1,
-        'imagem' => 'https://i5.walmartimages.com/asr/07c38307-9678-4450-b190-c0b384f4ed6c_1.fe881ef2cc4bb838e516fff313c4b559.jpeg'
+        'imagem' => 'https://cdn.dooca.store/2050/products/kwziqxeovj7ihvulzfw8iakzkwkvrvch2hev.png?v=1623705616000'
     ),
     array(
         'id' => $indice,
@@ -95,7 +95,7 @@ $products = array(
         'Nome' => 'Calça Legging',
         'Valor' => 34.99,
         'quantidade' => 1,
-        'imagem' => 'https://i5.walmartimages.com/asr/07c38307-9678-4450-b190-c0b384f4ed6c_1.fe881ef2cc4bb838e516fff313c4b559.jpeg'
+        'imagem' => 'https://cdn.awsli.com.br/600x450/1058/1058679/produto/131162865c8854ae768.jpg'
     ),
     array(
         'id' => $indice,
@@ -103,7 +103,7 @@ $products = array(
         'Nome' => 'Carteira',
         'Valor' => 24.99,
         'quantidade' => 1,
-        'imagem' => 'https://i5.walmartimages.com/asr/07c38307-9678-4450-b190-c0b384f4ed6c_1.fe881ef2cc4bb838e516fff313c4b559.jpeg'
+        'imagem' => 'https://images.tcdn.com.br/img/img_prod/1151305/carteira_couro_ecologico_origens_essencial_porta_cnh_1373_1_65211f5cc560c806488c935912446c25.jpg'
     ),
     array(
         'id' => $indice,
@@ -111,7 +111,7 @@ $products = array(
         'Nome' => 'Boné',
         'Valor' => 24.99,
         'quantidade' => 1,
-        'imagem' => 'https://i5.walmartimages.com/asr/07c38307-9678-4450-b190-c0b384f4ed6c_1.fe881ef2cc4bb838e516fff313c4b559.jpeg'
+        'imagem' => 'https://cea.vtexassets.com/arquivos/ids/10673138/Bone-Masculino-Aba-Curva-Cinza-8920174-Cinza_1.jpg?v=1772992383'
     ),
     array(
         'id' => $indice,
@@ -119,7 +119,7 @@ $products = array(
         'Nome' => 'Vestido',
         'Valor' => 59.99,
         'quantidade' => 1,
-        'imagem' => 'https://i5.walmartimages.com/asr/07c38307-9678-4450-b190-c0b384f4ed6c_1.fe881ef2cc4bb838e516fff313c4b559.jpeg'
+        'imagem' => 'https://voudevestido.com.br/358-large_default/vestido-maine.jpg'
     ),
     array(
         'id' => $indice,
@@ -127,7 +127,7 @@ $products = array(
         'Nome' => 'Moletom',
         'Valor' => 69.99,
         'quantidade' => 1,
-        'imagem' => 'https://i5.walmartimages.com/asr/07c38307-9678-4450-b190-c0b384f4ed6c_1.fe881ef2cc4bb838e516fff313c4b559.jpeg'
+        'imagem' => 'https://m.media-amazon.com/images/I/61kEqQpiL-L._AC_SL1000_.jpg'
     ),
     array(
         'id' => $indice,
@@ -135,7 +135,7 @@ $products = array(
         'Nome' => 'Pulseira',
         'Valor' => 9.99,
         'quantidade' => 20,
-        'imagem' => 'https://i5.walmartimages.com/asr/07c38307-9678-4450-b190-c0b384f4ed6c_1.fe881ef2cc4bb838e516fff313c4b559.jpeg'
+        'imagem' => 'https://images.tcdn.com.br/img/img_prod/1044174/pulseira_de_bebe_banhado_a_ouro_631_1_70263842a01a26cf01b7490236305690.jpg'
     ),
     array(
         'id' => $indice,
@@ -143,7 +143,7 @@ $products = array(
         'Nome' => 'Blusa',
         'Valor' => 39.99,
         'quantidade' => 1,
-        'imagem' => 'https://i5.walmartimages.com/asr/07c38307-9678-4450-b190-c0b384f4ed6c_1.fe881ef2cc4bb838e516fff313c4b559.jpeg'
+        'imagem' => 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoB2LaktPIHTpKn49oVRNkoL5ZoJeJxuuXzueK440FywymhfWl99u8XHMoGoUc-SyqHxU&usqp=CAU'
     ),
     array(
         'id' => $indice,
@@ -151,7 +151,7 @@ $products = array(
         'Nome' => 'Calçados Esportivos',
         'Valor' => 89.99,
         'quantidade' => 1,
-        'imagem' => 'https://i5.walmartimages.com/asr/07c38307-9678-4450-b190-c0b384f4ed6c_1.fe881ef2cc4bb838e516fff313c4b559.jpeg'
+        'imagem' => 'https://m.media-amazon.com/images/I/71lvX+cKxAL._AC_UF894,1000_QL80_.jpg'
     ),
     array(
         'id' => $indice,
@@ -159,7 +159,7 @@ $products = array(
         'Nome' => 'Cinto',
         'Valor' => 19.99,
         'quantidade' => 1,
-        'imagem' => 'https://i5.walmartimages.com/asr/07c38307-9678-4450-b190-c0b384f4ed6c_1.fe881ef2cc4bb838e516fff313c4b559.jpeg'
+        'imagem' => 'https://cdn.shopify.com/s/files/1/0602/3217/1757/products/Cinto-Masculino-Em-Couro-Soleta-Emburana-Preto-01_1000x.jpg?v=1668188423'
     )
 );
 
